416c3c811f8d835b66234d7b532f78962ead380f
MDBossss <ferrari020203@gmail.com>
2022-10-09 23:12:39 +0200

    Add files via upload

---
+++ LV1/priprema/src/hr/java/production/model/Factory.java
@@ -0,0 +1,40 @@
+package hr.java.production.model;
+
+public class Factory {
+    private String name;
+    private Address address;
+    private Item[] items; //array
+
+    public Factory(String name, Address address, Item[] items) {
+        this.name = name;
+        this.address = address;
+        this.items = items;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setItems(Item[] items) {
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+
+
+}
